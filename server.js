70f454cfdc5da5b231ed249854fdfbefa36332bd
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-10-31 14:58:01 +0100

    added gitignore

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
+const http = require("http");
+const https = require("https");
+const config = require("./config.json");
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const apiKeys = config.apiKeys;
+const validApiKey = "j176p6rf1qyy7vc7wxp7j4cw"
+// const validApiKey = config.validApiKey;
 
-// Funzione per ottenere la prossima chiave API nella rotazione
+let currentApiKeyIndex = 0;
+
 function getNextApiKey() {
-  let index = 0;
-  const numKeys = config.apiKeys.length;
-  return function() {
-    const apiKey = config.apiKeys[index];
-    index = (index + 1) % numKeys;
-    return apiKey;
+  const numKeys = apiKeys.length;
+  currentApiKeyIndex = (currentApiKeyIndex + 1) % numKeys;
+  const apiKeyInfo = apiKeys[currentApiKeyIndex];
+  return {
+    apiKey: apiKeyInfo.key,
+    apiUrl: apiKeyInfo.url,
+    models: apiKeyInfo.models,
   };
 }
 
-function forwardToOpenAI(url, data, apiKey, res) {
-  const openaiUrl = 'https://chimeragpt.adventblocks.cc/api' + url;
-
+function forwardToOpenAI(apiUrl, url, data, apiKey, res, models) {
+  const openaiUrl = apiUrl + url;
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + apiKey
-    }
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + encodeURIComponent(apiKey),
+    },
   };
-
   const req = https.request(openaiUrl, options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers); // Pass through response status and headers
-    proxyRes.pipe(res); // Pipe the proxy response directly to the client response
+    console.log(
+      "Received response from the reverse proxy. Status:",
+      proxyRes.statusCode
+    );
+
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res);
   });
 
-  req.on('error', (error) => {
-    console.error('Error sending request to OpenAI:', error);
+  req.on("error", (error) => {
+    console.error("Error sending request to OpenAI:", error);
     res.statusCode = 500;
     res.end();
   });
@@ -41,46 +48,79 @@ function forwardToOpenAI(url, data, apiKey, res) {
   req.end();
 }
 
-http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json'); // Set appropriate Content-Type
-  res.setHeader('Access-Control-Allow-Origin', '*');
+function checkModel(apiKey, apiUrl, models, model, url, data, res) {
+  if (models.includes(model)) {
+    console.log(`Forwarding to ${apiUrl} with API key: ${apiKey}`);
+    forwardToOpenAI(apiUrl, url, data, apiKey, res, models);
+    getNextApiKey();
+  } else {
+    getNextApiKey();
+    console.log("Model not supported by this API key");
+    res.statusCode = 400;
+    res.end(JSON.stringify({ error: "Model not supported by this API key" }));
+  }
+}
 
-  if (req.method !== 'POST') {
+http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  console.log("Received POST request:", req.url);
+
+  if (req.method !== "POST") {
     res.statusCode = 405; // Method Not Allowed
     res.end();
     return;
   }
 
   const chunks = [];
-  req.on('data', (chunk) => {
+  req.on("data", (chunk) => {
     chunks.push(chunk);
   });
 
-  req.on('end', () => {
+  req.on("end", () => {
     const data = Buffer.concat(chunks).toString();
-    const url = req.url.split('?')[0];
+    const url = req.url.split("?")[0];
 
     try {
+      const authorizationHeader = req.headers["authorization"];
+      const providedApiKey = authorizationHeader
+      ? authorizationHeader.split(" ")[1].trim()
+      : null;  
       const payload = JSON.parse(data);
-      const providedApiKey = payload.apiKey;
+      const model = payload.model;
 
-      if (!providedApiKey || providedApiKey !== config.validApiKey) {
+      console.log("Received API key:", providedApiKey);
+      console.log("Received model:", model);
+      console.log("validApiKey:", validApiKey);
+
+      if (!providedApiKey || providedApiKey.trim() !== validApiKey.trim()) {
+        console.log('Invalid API key');
         res.statusCode = 401;
         res.end(JSON.stringify({ error: 'Invalid API key' }));
         return;
-      }
-
-      if (url && url.includes('/v1/')) {
-        forwardToOpenAI(url, data, getNextApiKey(), res);
       } else {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ error: 'Not found' }));
+        const apiKeyInfo = config.apiKeys.find(
+          (apiKeyInfo) => apiKeyInfo.key === providedApiKey
+        );
+
+        if (apiKeyInfo) {
+          const apiKey = apiKeyInfo.key;
+          const apiUrl = apiKeyInfo.url;
+          const models = apiKeyInfo.models;
+
+          checkModel(apiKey, apiUrl, models, model, url, data, res);
+        } else {
+          console.log("Invalid API key");
+          res.statusCode = 401;
+          res.end(JSON.stringify({ error: "Invalid API key" }));
+        }
       }
     } catch (error) {
+      console.error("Error processing request:", error);
       res.statusCode = 400; // Bad Request
-      res.end(JSON.stringify({ error: 'Invalid JSON payload' }));
+      res.end(JSON.stringify({ error: "Invalid JSON payload" }));
     }
   });
-}).listen(3000, 'localhost', () => {
-  console.log('Server running at http://localhost:3000/');
+}).listen(3456, "localhost", () => {
+  console.log("Server running at http://localhost:3456/");
 });
